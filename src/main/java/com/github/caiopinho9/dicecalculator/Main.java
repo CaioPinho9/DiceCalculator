63be59b3170bdeeec856c8f7c0e781305139f018
CaioPinho9 <69582667+CaioPinho9@users.noreply.github.com>
2022-03-10 14:01:37 -0300

    Update Main.java

--- src/main/java/com/github/caiopinho9/dicecalculator/Main.java
+++ src/main/java/com/github/caiopinho9/dicecalculator/Main.java
@@ -1,17 +1,10 @@
 package com.github.caiopinho9.dicecalculator;
 
-import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.jar.JarInputStream;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("caralho");
-
         String expression = "3d4";
         Operator operator = new Operator(expression);
         final String title = "Dice Calculator";
